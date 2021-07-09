5c775440dfa77b8c20c471d8d286b83d5e7f602b
gulshan <jankarikibaat@gmail.com>
2021-07-09 18:41:56 +0530

    added the basic layout of music player app.

--- app/src/main/java/com/example/gaanabaja/MainActivity.kt
+++ app/src/main/java/com/example/gaanabaja/MainActivity.kt
@@ -9,13 +9,30 @@ import com.example.gaanabaja.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var paused = true /* this variable will tell that song is playing or it is paused*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        binding.testButton.setOnClickListener {
-            Toast.makeText(this,"Ruko zara sabr karo",Toast.LENGTH_LONG).show()
+        binding.playButton.setOnClickListener{
+            if (paused){
+                Toast.makeText(this,"Playing...",Toast.LENGTH_SHORT).show()
+                binding.playButton.setImageResource(R.drawable.ic_pause)
+                paused = false
+            }else{
+                Toast.makeText(this,"Paused",Toast.LENGTH_SHORT).show()
+                binding.playButton.setImageResource(R.drawable.ic_play)
+                paused = true
+            }
+        }
+
+        binding.skipPreviousButton.setOnClickListener {
+            Toast.makeText(this,"You can't skip this one",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.skipNextButton.setOnClickListener {
+            Toast.makeText(this,"You can't skip this one",Toast.LENGTH_SHORT).show()
         }
     }
 }
